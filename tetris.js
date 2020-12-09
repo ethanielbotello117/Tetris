cd15b1d3a4d363a69046b47f9f31d4b394b8068e
Ethaniel Botello <ethanielbotello117@gmail.com>
2020-12-09 14:44:29 -0700

    java tweaks

--- tetris.js
+++ tetris.js
@@ -1,37 +1,17 @@
+// Row and columns array
+
+
+for(let i = 0; i <= 10; i++){
+
+}
+
+// Play area
+
+
 // Main play area piece
-C15.style.background = 'blue';
-C16.style.background = 'blue';
-D16.style.background = 'blue';
-E16.style.background = 'blue';
 
-C15.style.border = 'solid white 1px';
-C16.style.border = 'solid white 1px';
-D16.style.border = 'solid white 1px';
-E16.style.border = 'solid white 1px';
-
-C15.style.opacity = '100%';
-C16.style.opacity = '100%';
-D16.style.opacity = '100%';
-E16.style.opacity = '100%';
 
 // Held piece display
-PR24.style.background = 'green';
-PR34.style.background = 'green';
-PR44.style.background = 'green';
-PR54.style.background = 'green';
 
-PR24.style.border = 'solid white 1px';
-PR34.style.border = 'solid white 1px';
-PR44.style.border = 'solid white 1px';
-PR54.style.border = 'solid white 1px';
 
 // Next piece display
-NPR34.style.background = 'red';
-NPR43.style.background = 'red';
-NPR44.style.background = 'red';
-NPR45.style.background = 'red';
-
-NPR34.style.border = 'solid white 1px';
-NPR43.style.border = 'solid white 1px';
-NPR44.style.border = 'solid white 1px';
-NPR45.style.border = 'solid white 1px';
